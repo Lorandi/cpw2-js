a194e51d488be765c2c6cc48f5ac3df3cef50501
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2023-03-24 14:21:17 +0000

    soluciona problemas

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-
 /**
  * Recebe um array com os nomes de pessoas e retorna um array invertido
  *
  * @param {*} people : Um array contendo um conjunto de nomes
  * @returns Um array com os nomes invertidos
  */
-function invert(people){
+function invert(people) {
+  let nomesInvertidos = [];
+  for (let i = people.length - 1; i >= 0; i--) {
+    nomesInvertidos.push(people[i]);
+  }
 
-    // TODO
-    // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
-    // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
-    // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
-    // nomes dos usuários.
+  for (let i = 0; i < nomesInvertidos.length; i++) {
+    console.log(nomesInvertidos[i]);
+  }
 
-    return [];
+  return nomesInvertidos;
 }
 
 /**
@@ -22,14 +23,12 @@ function invert(people){
  * @param {*} grades : Um array de notas
  * @returns Uma média a partir do array de notas
  */
-function mean(grades){
-
-    // TODO
-    // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
-
-    return 0;
+function mean(grades) {
+  let total = 0;
+  for (let i = 0; i < grades.length; i++) {
+    total += grades[i];
+  }
+  return total / grades.length;
 }
 
 /**
@@ -38,14 +37,12 @@ function mean(grades){
  * @param {*} mean : Um número que representa uma média
  * @returns Uma string dizendo se um estudante está aprovado ou não
  */
-function isApproved(mean){
-
-    // TODO
-    // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
-
-    return "";
+function isApproved(mean) {
+  if (mean >= 7) {
+    return "aprovado";
+  } else {
+    return "reprovado";
+  }
 }
 
 /**
@@ -54,21 +51,96 @@ function isApproved(mean){
  * @param {*} strDate : Uma string no formato de data
  * @returns Uma descrição da data informada
  */
-function wide(strDate){
-
-    // TODO
-    // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
-    // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
-    // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
-    // Nesse caso, o separador é a barra (/) da data.
-
+function wide(strDate) {
+  if (strDate === "") {
     return "";
+  }
+
+  let mesPorExtenso;
+  strDate = strDate.split("/");
+  let dia = strDate[0];
+  let mes = strDate[1];
+  let ano = strDate[2];
+
+  if (isNaN(dia) || isNaN(mes) || isNaN(ano)) {
+    return "";
+  } else {
+    let diaNumerico = parseInt(dia);
+    let mesNumerico = parseInt(mes);
+    let anoNumerico = parseInt(ano);
+    if (
+        diaNumerico < 1 ||
+        diaNumerico > 31 ||
+        mesNumerico < 1 ||
+        mesNumerico > 12 ||
+        anoNumerico < 0
+    ) {
+      return "";
+    }
+  }
+
+  switch (mes) {
+    case "01":
+      mesPorExtenso = "janeiro";
+      break;
+
+    case "02":
+      mesPorExtenso = "fevereiro";
+      break;
+
+    case "03":
+      mesPorExtenso = "março";
+      break;
+
+    case "04":
+      mesPorExtenso = "abril";
+      break;
+
+    case "05":
+      mesPorExtenso = "maio";
+      break;
+
+    case "06":
+      mesPorExtenso = "junho";
+      break;
+
+    case "07":
+      mesPorExtenso = "julho";
+      break;
+
+    case "08":
+      mesPorExtenso = "agosto";
+      break;
+
+    case "09":
+      mesPorExtenso = "setembro";
+      break;
+
+    case "10":
+      mesPorExtenso = "outubro";
+      break;
+
+    case "11":
+      mesPorExtenso = "novembro";
+      break;
+
+    case "12":
+      mesPorExtenso = "dezembro";
+      break;
+
+    default:
+      mesPorExtenso = mes;
+      break;
+  }
+
+  let data = dia + " de " + mesPorExtenso + " de " + ano;
+  return data;
 }
 
 // Trecho utilizado pelos testes
 exports._test = {
-    invert: invert,
-    mean: mean,
-    isApproved: isApproved,
-    wide: wide
-}
+  invert: invert,
+  mean: mean,
+  isApproved: isApproved,
+  wide: wide,
+};
